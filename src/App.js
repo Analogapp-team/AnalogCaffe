3deb2174da1dd443ad7efc6fc9ebc49e07144b15
EmilEngbergAAU <eengbe20@student.aau.dk>
2025-11-27 11:39:50 +0100

    Router note for victor

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/explore" element={<Explore />} />
             <Route path="/profile" element={<Profile />} />
-            <Route path="/profile/:userId" element={<Profile />} />
+            <Route path="/profile/:userId" element={<Profile />} /> {/* Victor use this router for the explore page (you were right) */}
             <Route path="/profile/settings" element={<ProfileSettings />} />
             <Route path="/Events" element={<Events />} />
           </Route>
