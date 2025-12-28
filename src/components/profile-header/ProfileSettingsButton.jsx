af67c56321f58a2316f50563a1cfce423246f26a
Adrian Klobucnik <adrianklobucnik@gmail.com>
2025-12-28 17:22:23 +0100

    Add global ui-button usage for events and my profile page

--- src/components/profile-header/ProfileSettingsButton.jsx
+++ src/components/profile-header/ProfileSettingsButton.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import "./ProfileSettingsButton.css";
 import settingsIcon from "../../assets/icons/settings.svg";
 
 const ProfileSettingsButton = ({ onClick }) => {
   return (
-    <button className="profile-settings-btn" onClick={onClick}>
-      <img src={settingsIcon} alt="Settings icon" className="settings-icon" />
+    <button
+      className="ui-button ui-button--secondary ui-button--with-icon"
+      onClick={onClick}
+    >
+      <img src={settingsIcon} alt="Settings" />
       Profile settings
     </button>
   );
